df0299018dac45a052f774b115ab863c56d11b3c
Christian <battlebeast89@gmx.net>
2025-05-28 22:01:25 +0200

    set db

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,9 +1,42 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm'; // Import TypeORM if needed in the future
+import Joi from 'joi';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      validationSchema: Joi.object({
+        NODE_ENV: Joi.string().valid('development', 'production').required(),
+        DB_PASSWORD: Joi.string()
+          .pattern(
+            new RegExp(
+              '^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[!@#$%^&*()_+\\-=[\\]{};:\'",.<>/?]).{8,}$',
+            ),
+          )
+          .required()
+          .messages({
+            'string.pattern.base':
+              'DB_PASSWORD must be at least 8 characters long and include uppercase, lowercase, number, and special character.',
+          }),
+      }),
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => ({
+        type: 'mysql',
+        host: 'localhost',
+        port: 3306,
+        username: 'root',
+        password: configService.get('DB_PASSWORD'),
+        database: 'trading',
+        entities: [],
+        synchronize: false,
+      }),
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
