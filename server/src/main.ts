b3e0a7f9daf80283b051b745704fd54237993e76
Christian Wechsel <c.wechsel@web.de>
2025-05-29 15:29:02 +0200

    feat(https): configure HTTPS options with self-signed certificates

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,9 +1,15 @@
 import { NestFactory } from '@nestjs/core';
+import { readFileSync } from 'fs';
 import helmet from 'helmet';
+import { join } from 'path';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const httpsOptions = {
+    key: readFileSync(join(__dirname, '../certificates/localhost.key')),
+    cert: readFileSync(join(__dirname, '../certificates/localhost.cert')),
+  };
+  const app = await NestFactory.create(AppModule, { httpsOptions });
   app.use(helmet());
   await app.listen(process.env.PORT ?? 3000);
 }
