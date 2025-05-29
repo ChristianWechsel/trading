50e726bacccb2ccdd285f7c998dafc1729e858e8
Christian Wechsel <c.wechsel@web.de>
2025-05-29 11:21:12 +0200

    add bcrypt for password hashing and implement user registration endpoint

--- server/src/auth/auth.controller.ts
+++ server/src/auth/auth.controller.ts
@@ -1,4 +1,9 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+  @Post('register')
+  register(@Body() body: { username: string; password: string }) {
+    console.log(body);
+  }
+}
