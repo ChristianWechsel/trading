aef66cd83e1c9e640005d8d2623083280bcdf587
Christian Wechsel <c.wechsel@web.de>
2025-05-29 20:37:46 +0200

    feat(validation): enhance global validation pipe with whitelist and transform options

--- server/src/main.ts
+++ server/src/main.ts
@@ -12,7 +12,7 @@ async function bootstrap() {
   };
   const app = await NestFactory.create(AppModule, { httpsOptions });
   app.use(helmet());
-  app.useGlobalPipes(new ValidationPipe());
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
