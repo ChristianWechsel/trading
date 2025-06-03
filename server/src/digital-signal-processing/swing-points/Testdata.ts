b838bcf7a6a855e3e294e594037a18508fd9740c
Christian Wechsel <c.wechsel@web.de>
2025-06-03 22:16:02 +0200

    feat(tests): enhance swing points analysis with additional test cases and update Jest configuration

--- server/src/digital-signal-processing/swing-points/Testdata.ts
+++ server/src/digital-signal-processing/swing-points/Testdata.ts
@@ -3,7 +3,7 @@ import { SwingPointData } from './swing-points.interface';
 
 type SwingPointAnalysis = {
   data: DataPoint[];
-  result: SwingPointData;
+  result: SwingPointData[];
 };
 
 export class TestDataSwingPoints {
@@ -14,10 +14,12 @@ export class TestDataSwingPoints {
         { x: 2, y: 3 },
         { x: 3, y: 2 },
       ],
-      result: {
-        swingPointType: 'swingHigh',
-        point: { x: 2, y: 3 },
-      },
+      result: [
+        {
+          swingPointType: 'swingHigh',
+          point: { x: 2, y: 3 },
+        },
+      ],
     };
   }
 
@@ -28,10 +30,41 @@ export class TestDataSwingPoints {
         { x: 2, y: 2 },
         { x: 3, y: 3 },
       ],
-      result: {
-        swingPointType: 'swingLow',
-        point: { x: 2, y: 2 },
-      },
+      result: [
+        {
+          swingPointType: 'swingLow',
+          point: { x: 2, y: 2 },
+        },
+      ],
+    };
+  }
+
+  noSwingPoint(): SwingPointAnalysis {
+    return {
+      data: [
+        { x: 1, y: 1 },
+        { x: 2, y: 1 },
+        { x: 3, y: 1 },
+      ],
+      result: [],
+    };
+  }
+
+  multipleSwingPoints(): SwingPointAnalysis {
+    return {
+      data: [
+        { x: 1, y: 1 },
+        { x: 2, y: 3 },
+        { x: 3, y: 2 },
+        { x: 4, y: 4 },
+        { x: 5, y: 3 },
+      ],
+      result: [
+        { swingPointType: 'swingHigh', point: { x: 2, y: 3 } },
+        { swingPointType: 'swingLow', point: { x: 3, y: 2 } },
+        { swingPointType: 'swingHigh', point: { x: 4, y: 4 } },
+        { swingPointType: 'swingLow', point: { x: 5, y: 3 } },
+      ],
     };
   }
 }
--- server/src/digital-signal-processing/swing-points/swing-points.spec.ts
+++ server/src/digital-signal-processing/swing-points/swing-points.spec.ts
@@ -7,12 +7,24 @@ describe('SwingPoints', () => {
   it('detect swing high', () => {
     const singleSwingHigh = testData.singleSwingHigh();
     const swingPoints = new SwingPoints(singleSwingHigh.data);
-    expect(swingPoints.getSwingPoints()).toEqual([singleSwingHigh.result]);
+    expect(swingPoints.getSwingPoints()).toEqual(singleSwingHigh.result);
   });
 
   it('detect swing low', () => {
     const singleSwingLow = testData.singleSwingLow();
     const swingPoints = new SwingPoints(singleSwingLow.data);
-    expect(swingPoints.getSwingPoints()).toEqual([singleSwingLow.result]);
+    expect(swingPoints.getSwingPoints()).toEqual(singleSwingLow.result);
+  });
+
+  it('detect no swing points', () => {
+    const noSwingPoint = testData.noSwingPoint();
+    const swingPoints = new SwingPoints(noSwingPoint.data);
+    expect(swingPoints.getSwingPoints()).toEqual(noSwingPoint.result);
+  });
+
+  it('detect multiple swing points', () => {
+    const multipleSwingPoints = testData.multipleSwingPoints();
+    const swingPoints = new SwingPoints(multipleSwingPoints.data);
+    expect(swingPoints.getSwingPoints()).toEqual(multipleSwingPoints.result);
   });
 });
