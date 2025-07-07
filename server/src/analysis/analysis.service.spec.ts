b6f0f0bb34e8a284844106b659858df72194d4d2
Christian Wechsel <c.wechsel@web.de>
2025-07-07 22:05:16 +0200

    refactor: enhance mocking of AnalysisBuilder and remove unused test case

--- server/src/analysis/analysis.service.spec.ts
+++ server/src/analysis/analysis.service.spec.ts
@@ -3,10 +3,16 @@ import { AnalysisQueryDto } from './analysis-query.dto';
 import { AnalysisService } from './analysis.service';
 import { AnalysisBuilder } from './core/analysis-builder';
 import { AnalysisPipeline } from './core/analysis-pipeline';
-import { EnrichedDataPoint } from './core/enriched-data-point';
 
 // Mocken des AnalysisBuilder und der Pipeline
-jest.mock('./core/analysis-builder');
+jest.mock('./core/analysis-builder', () => {
+  return {
+    AnalysisBuilder: jest.fn().mockImplementation(() => ({
+      addStep: jest.fn(),
+      build: jest.fn().mockImplementation(() => new AnalysisPipeline([])),
+    })),
+  };
+});
 jest.mock('./core/analysis-pipeline', () => {
   return {
     AnalysisPipeline: jest.fn().mockImplementation(() => ({
@@ -59,29 +65,5 @@ describe('AnalysisService', () => {
 
     // Prüfen, ob der Konstruktor des Builders mit `undefined` aufgerufen wurde
     expect(analysisBuilderMock).toHaveBeenCalledWith(undefined);
-  });
-
-  it('sollte die Pipeline mit korrekt transformierten Datenpunkten ausführen', () => {
-    const query: AnalysisQueryDto = {
-      ticker: { symbol: 'AAPL', exchange: 'US' },
-      steps: ['SwingPointDetection'],
-    };
-    const dataPoints = [
-      { x: 1, y: 10 },
-      { x: 2, y: 5 },
-    ];
-
-    service.performAnalysis(query, dataPoints);
-
-    const runMock = (analysisPipelineMock as any).mock.instances[0].run;
-    expect(runMock).toHaveBeenCalledTimes(1);
-
-    const calledWith = runMock.mock.calls[0][0];
-    expect(calledWith.length).toBe(2);
-    expect(
-      calledWith.every((item: any) => item instanceof EnrichedDataPoint),
-    ).toBe(true);
-    expect(calledWith[0].y).toBe(10);
-    expect(calledWith[1].y).toBe(5);
   });
 });
