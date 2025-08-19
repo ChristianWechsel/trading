c64e1a7660d4b824ea1138dcbd5c2865e197f6c4
Christian Wechsel <c.wechsel@web.de>
2025-08-19 22:40:00 +0200

    refactor: simplify position management and remove outdated comments in Portfolio class

--- server/src/analysis/core/portfolio/portfolio.ts
+++ server/src/analysis/core/portfolio/portfolio.ts
@@ -3,13 +3,6 @@ import { Account } from '../account/account';
 import { Position, PriceDateInfo, Stops } from '../position/position';
 import { TransactionData } from '../transaction/transaction';
 
-// Portfolio besteht aus mehrern Positionen. Jede Position verwaltet alle Transaktionen
-// offene und geschlossende Positionen verwerfen. Jede Position muss für sich selbst entscheiden, ob gekauft, verkauft oder
-// über stops Aktion ausgelöst wird. Dies muss Portfolio nicht kümmern
-// Es muss für jede Iteration der aktuelle Preis an Position weitergebgen werden,
-// damit die stops korrekt ausgelöst werden können
-// Ebenso müssen die Strategiesignale an Position weitergegeben werden
-
 export class Portfolio {
   private positions: Position[];
 
@@ -19,14 +12,22 @@ export class Portfolio {
 
   addPosition(ticker: TickerDto) {
     if (!this.hasPosition(ticker)) {
-      this.positions.push(new Position(ticker));
+      this.positions.push(
+        new Position(ticker, {
+          buy: (shares, price) => {
+            this.account.debit(price * shares);
+          },
+          sell: (shares, price) => {
+            this.account.credit(price * shares);
+          },
+        }),
+      );
     }
   }
 
   placeOrder(ticker: TickerDto, order: TransactionData) {
     const position = this.getPosition(ticker);
     if (position) {
-      this.updateAccountBalance(order);
       position.placeOrder(order);
     }
   }
@@ -45,22 +46,15 @@ export class Portfolio {
     }
   }
 
+  getTransactions() {
+    return this.positions.flatMap((p) => p.getTransactions());
+  }
+
   private hasPosition(ticker: TickerDto) {
     return this.positions.some((p) => p.isPosition(ticker));
   }
 
   private getPosition(ticker: TickerDto) {
     return this.positions.find((p) => p.isPosition(ticker));
-  }
-
-  private updateAccountBalance(order: TransactionData) {
-    switch (order.type) {
-      case 'buy':
-        this.account.debit(order.price * order.shares);
-        break;
-      case 'sell':
-        this.account.credit(order.price * order.shares);
-        break;
-    }
   }
 }
